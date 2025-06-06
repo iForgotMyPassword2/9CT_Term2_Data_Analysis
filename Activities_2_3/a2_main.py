75d52446441e21631073407d6e0662d1f2d1f60a
iForgotMyPassword2 <warriermanas@gmail.com>
2025-06-06 11:58:37 +1000

    Completed Filtering, sorting and grouping
    Completed filtering, sorting and grouping activities and answered all required questions in md file.

--- Activities_2_3/a2_main.py
+++ Activities_2_3/a2_main.py
@@ -13,3 +13,45 @@ print(df['Sport'].value_counts().head())
 print(df['Sex'].value_counts())
 
 print(df.describe())
+
+# Filter for female athletes only
+female_athletes = df[df['Sex'] == 'F']
+print(female_athletes.head())
+
+# Filter for athletes older than 35
+older_athletes = df[df['Age'] > 35]
+print(older_athletes[['Name', 'Age', 'Sport']].head())
+
+# print(len(female_athletes))
+# print(len(older_athletes))
+
+# Female athletes over 30
+combo_filter = df[(df['Sex'] == 'F') & (df['Age'] > 30)]
+print(combo_filter[['Name', 'Age', 'Sport']].head())
+
+# Male athletes in Basketball
+basketball_males = df[(df['Sex'] == 'M') & (df['Sport'] == 'Basketball')]
+print(basketball_males.head())
+
+# Australian Athletes in Swimming:
+australian_swimming = df[(df['Sport'] == 'Swimming') & (df['Team'] == 'Australia')]
+print(australian_swimming)
+
+# sorting data:
+
+
+# Sort by age
+sorted_by_age = df.sort_values(by='Age', ascending=False)
+print(sorted_by_age[['Name', 'Age', 'Sport']].head())
+
+# Sort by weight
+sorted_by_weight = df.sort_values(by='Weight', ascending=False)
+print(sorted_by_weight[['Name', 'Weight', 'Sport']].head())
+
+# Count participants in each sport
+sport_counts = df['Sport'].value_counts()
+print(sport_counts.head())
+
+# Count medals per team
+medals_by_team = df[df['Medal'].notnull()].groupby('Team')['Medal'].count()
+print(medals_by_team.sort_values(ascending=False).head())
